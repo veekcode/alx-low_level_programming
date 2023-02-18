96954ee9bec31cb3c1607a1147d2fb74ab2df9ee
veekcodes <veekcodes@gmail.com>
2023-02-19 00:08:16 +0100

    the end of the task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#!/bin/bas
+#include <stdio.h>
+
+int main(0)
+{
+    printf("%lu\n", sizeof(char));
+    printf("%lu\n", sizeof(int));
+    printf("%lu\n", sizeof(float));
+    printf("%lu", sizeof(double));
+    
+    return 0;
+}    
+
