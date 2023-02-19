540d25ebd472f551da06d8fb013adde0aa99f271
veekcodes <veekcodes@gmail.com>
2023-02-19 16:09:46 +0100

    now in printf statement

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
-int main (0)
-{
-	printf("with proper grammar, but the outcome is a piece of art,");
+/**
+ *   main - entry point
+ *
+ *   Return: Always 0 (Success)
+ */
 
-	Return (0);
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
