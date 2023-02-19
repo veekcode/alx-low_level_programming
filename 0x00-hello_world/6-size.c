1c726280e72a3003957fe69dcfb32179c257f35f
veekcodes <veekcodes@gmail.com>
2023-02-19 17:38:33 +0100

    the size of my files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,18 @@
-#!/bin/bas
 #include <stdio.h>
+/**
+ *   main - entry point
+ *
+ *   Return: Always 0 (Success)
+ */
 
-int main(0)
+int main(void)
 {
-    printf("%lu\n", sizeof(char));
-    printf("%lu\n", sizeof(int));
-    printf("%lu\n", sizeof(float));
-    printf("%lu", sizeof(double));
-    
-    return 0;
-}    
+    printf("Size of a char: %zu byte(s)\n", sizeof(char));
+    printf("size of an int: %zu byte(s)\n", sizeof(int));
+    printf("size of a long: %zu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("size of a float: %zu byte(s)\n", sizeof(float));
+
+    return (0);
+}
 
