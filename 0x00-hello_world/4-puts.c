a8aa4809866583c9d591378095cfcaf45398d8d8
veekcodes <veekcodes@gmail.com>
2023-02-19 14:50:28 +0100

    the end of the task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ *   main - entry point
+ *
+ *   Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
-	
-	return 0;
+
+	return (0);
 }
