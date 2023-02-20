1330349a17cacd0ec08a01a015cecebf9e6c0d91
veekcodes <veekcodes@gmail.com>
2023-02-20 17:33:27 +0100

    made some corrections

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,12 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("%d\n" , n);
 if (n > 0){
-printf("%d is positive.\n");
+printf("%d is positive.\n", n);
 }else if(n < 0) {
-printf("%d is negative.\n");
+printf("%d is negative.\n", n);
 } else {
-printf("%d is zero.\n");
+printf("%d is zero.\n", n);
 }
 
 return (0);
