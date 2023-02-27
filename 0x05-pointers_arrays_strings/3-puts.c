0bc4d10b8d2cbb16994d30aea8d1a9ada346a6ea
veekcodes <veekcodes@gmail.com>
2023-02-27 16:38:28 +0100

    just printed a line of characters

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <main.c>
+
+/**
+ * _puts - prints a string, followed by a new line .
+ * @str: validate the character
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
