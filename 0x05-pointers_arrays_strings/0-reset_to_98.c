f0d5ec3dfc72c571be7245e232e56ca5e784fb6f
veekcodes <veekcodes@gmail.com>
2023-02-27 15:53:44 +0100

    reseting the universe

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98.
+ * reset_to_98 - takes a pointer to an int and updates the value to 98.
  * @n : n is the pointer of n and return value 98
  * Return: Always 0.
  */
