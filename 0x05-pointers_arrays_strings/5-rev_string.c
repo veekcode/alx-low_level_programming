d1ba6daac21617285f3b39c08b5661c70394516d
veekcodes <veekcodes@gmail.com>
2023-02-27 17:51:47 +0100

    stylistic reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - prints reverse characters.
+ * @s: validates the character
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i = 0, c = 0, Aux;
+
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i > a)
+	{
+		Aux = s[i];
+		s[i--] = s[c];
+		s[a++] = Aux;
+	}
+}
