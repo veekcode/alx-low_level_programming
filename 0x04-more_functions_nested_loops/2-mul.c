23661aeda5de785ae9cdc9d4b5343f13944d69ad
veekcodes <veekcodes@gmail.com>
2023-02-28 23:34:39 +0100

    getting rid of more bugs

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- *-mul - prints the product of two integers
- *@input
- *Return mul
+ *mul - prints the product of two integers
+ *@a: a - Variable
+ *@b: b - Variable
+ *Return: Always 0
  */
 int mul(int a, int b)
 {
