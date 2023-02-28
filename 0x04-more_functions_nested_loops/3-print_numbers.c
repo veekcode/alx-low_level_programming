b41923f57b021023e5aeba18dabbe6b8a0ae7b25
veekcodes <veekcodes@gmail.com>
2023-03-01 00:00:11 +0100

    getting rid of more bugs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- *-print_numbers - Prints numbers from 0 to 9
- *@inputs
- *Return 0
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ *Return: Always 0
  */
 void print_numbers(void)
 {
 int num;
-for (num >= 0; numm <= 9; num++ )
+for (num >= 48; num <= 58; num++)
 {
 _putchar(num);
 }
 {
 _putchar('\n');
 }
-return 0
+return (0);
 }
