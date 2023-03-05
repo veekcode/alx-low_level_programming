11eca86860599eda254c07c125837c6912e27ea8
kingzLemuel <kingzLemuel@example.com>
2023-03-05 21:54:30 +0100

    nested corrections

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
+
 /**
- *main - check code
+ * main - entry point
  *
- *Return: Always 0 (Success)
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('e');
-_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-_putchar('\n');
-return (0);
+	return (0);
 }
